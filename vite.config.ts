b2ae630eef330f65b3b67b7ef3229c3784c71c49
kadiryazici <kyzc411@outlook.com.tr>
2023-11-21 22:59:03 +0300

    Add types

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
+import DTS from 'vite-plugin-dts'
 
 export default defineConfig({
+  plugins: [
+    DTS({
+      exclude: ['src']
+    })
+  ],
   build: {
     lib: {
       entry: fileURLToPath(new URL('/lib/lib.ts', import.meta.url)),
